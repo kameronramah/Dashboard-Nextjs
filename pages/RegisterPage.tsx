6f637e60efdc87ee32d1580f692bdd137d3d14ea
romai <romainlerme@hotmail.fr>
2022-06-01 10:03:32 +0200

    ajout page login, register, forgetPassword

---
+++ pages/RegisterPage.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Register from './components/Register'
+
+
+
+const RegisterPage: NextPage = () => {
+  return (
+    <div className="container-fluid">
+        <Register/>
+    </div>
+  )
+}
+
+export default RegisterPage
