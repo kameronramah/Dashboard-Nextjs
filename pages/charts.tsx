ec57b37b717c54ec8e5411951b07ce07ea2b4756
kameronramah <kramaherison@gmail.com>
2022-06-01 10:40:12 +0200

    ajout page charts

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -1,75 +1,67 @@
-import Image from 'next/image'
-import NavBar from './components/NavBar'
-import SideNav from './components/SideNav'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import NavBar from './components/NavBar';
+import SideNav from './components/SideNav';
+import ChartLine from './components/Charts/ChartLine';
+import ChartBar from './components/Charts/ChartBar';
+import ChartPie from './components/Charts/ChartPie';
 
-
-const Charts = () => {
+const Home: NextPage = () => {
+  return (
     <div className="sb-nav-fixed">
-      {/* side nav 
-      navbar 
-      main */}
       <NavBar/>
       <div id="layoutSidenav">
       <SideNav/>
-
-
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by editing{' '}
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
+          <main>
+            <div id="layoutSidenav_content">
+              <div className="container-fluid px-4">
+                  <h1 className="mt-4">Charts</h1>
+                  <ol className="breadcrumb mb-4">
+                      <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                      <li className="breadcrumb-item active">Charts</li>
+                  </ol>
+                  <div className="card mb-4">
+                      <div className="card-body">
+                          Chart.js is a third party plugin that is used to generate the charts in this template. The charts below have been customized - for further customization options, please visit the official
+                          <a target="_blank" href="https://www.chartjs.org/docs/latest/">Chart.js documentation</a>
+                          .
+                      </div>
+                  </div>
+                  <div className="card mb-4">
+                      <div className="card-header">
+                          <i className="fas fa-chart-area me-1"></i>
+                          Area Chart Example
+                      </div>
+                      <div className="card-body"><ChartLine height={30} width={'100%'}/></div>
+                      <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                  </div>
+                  <div className="row">
+                      <div className="col-lg-6">
+                          <div className="card mb-4">
+                              <div className="card-header">
+                                  <i className="fas fa-chart-bar me-1"></i>
+                                  Bar Chart Example
+                              </div>
+                              <div className="card-body"><ChartBar height={50} width={'100%'}/></div>
+                              <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                          </div>
+                      </div>
+                      <div className="col-lg-6">
+                          <div className="card mb-4">
+                              <div className="card-header">
+                                  <i className="fas fa-chart-pie me-1"></i>
+                                  Pie Chart Example
+                              </div>
+                              <div className="card-body"><ChartPie height={50} width={'100%'}/></div>
+                              <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </main>
       </div>
-
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
+  )
 }
 
-export default Charts;
+export default Home;
